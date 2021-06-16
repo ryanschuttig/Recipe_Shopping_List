a16ced658b2ea6cdb190489a5d716b45ab8be430
Ryan Schuttig <ryan.schuttig@capco.com>
2021-06-16 12:07:21 -0400

    passed data from recipes to shopping list via service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,47 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 import { Recipe } from './recipe.model';
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is a test', 'https://cdn.newseasonsmarket.com/wp-content/uploads/2015/11/Homepage-Recipe-Text.png'),
-        new Recipe('Another Test Recipe', 'This is a test', 'https://cdn.newseasonsmarket.com/wp-content/uploads/2015/11/Homepage-Recipe-Text.png')
+        new Recipe(
+            'Garden Salad',
+             'Simple and delicious!',
+              'https://cdn3.tmbi.com/toh/GoogleImagesPostCard/Armenian-Garden-Salad_exps47594_TH2236620A05_31_3bC_RMS.jpg',
+              [
+                  new Ingredient('Lettuce', 1),
+                  new Ingredient('Tomatoes', 5),
+                  new Ingredient('Onions', 1),
+                  new Ingredient('Cucumbers', 1)
+              ]),
+        new Recipe(
+            'Beyond Burger',
+             'Our take on a vegan burger!',
+              'https://gp1.com/wp-content/uploads/2018/08/Hamburger.jpg',
+              [
+                new Ingredient('Lettuce', 1),
+                new Ingredient('Tomatoes', 1),
+                new Ingredient('Onions', 1),
+                new Ingredient('Buns', 2),
+                new Ingredient('Ketchup', 1),
+                new Ingredient('Mustard', 1),
+                new Ingredient('Pickles', 1),
+                new Ingredient('Beyond Burger', 1),
+              ])
       ];
+
+      constructor(private slService: ShoppingListService) {}
 
       getRecipes() {
           return this.recipes.slice();
+      }
+
+      addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
       }
 }
