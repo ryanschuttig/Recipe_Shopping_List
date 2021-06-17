c25fc53b0075c11b3c3ab2c814cbdde259ddf80b
Ryan Schuttig <ryan.schuttig@capco.com>
2021-06-17 11:05:35 -0400

    added subjects

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
+
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
@@ -6,7 +7,6 @@ import { Recipe } from './recipe.model';
 
 @Injectable()
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
         new Recipe(
